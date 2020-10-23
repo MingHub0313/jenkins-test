05381b18ea2b9f96c25ff38cb86bb40876c10086
zhangmingming <zhangmingmig@adpanshi.com>
2020-10-23 10:20:22 +0800

    提交信息

---
+++ src/main/java/com/zmm/jenkins/JenkinsTestApplication.java
@@ -0,0 +1,17 @@
+package com.zmm.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @Name JenkinsTestApplication
+ * @Author 900045
+ * @Created by 2020/10/23 0008
+ */
+@SpringBootApplication
+public class JenkinsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestApplication.class, args);
+	}
+
+}
